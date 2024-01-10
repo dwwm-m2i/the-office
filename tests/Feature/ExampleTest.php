3571bdd61d79a598b0be0f1bfaf456328060c5b5
Slimtouflo <slimtouflo@.com>
2024-01-10 11:30:53 +0100

    migration

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,11 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_hello_route_returns_hello()
+    {
+        $response = $this->get('/hello');
+
+        $response->assertSee('<h1>Hello Toto</h1>', false);
+    }
 }
