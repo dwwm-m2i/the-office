4654bd538b7a4675bb537256a79fed31baab9443
Matthieu Mota <matthieumota@gmail.com>
2024-01-10 17:04:37 +0100

    Home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app')
 
-    <title>page d'accueil</title>
-</head>
-<body>
-
+@section('content')
     <h1>the office </h1>
-   
-
-
-    
-</body>
-</html>
+@endsection
