da6e1f5ab192c735628634e715664e5c5e001144
NESRINEbel <nesreenbel@gmail.com>
2024-01-10 10:22:08 +0100

    Nesrine

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,12 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_hello_route_returns_hello()
+    {
+        $response= $this ->get('/hello');
+        $response->assertSee('Hello Toto');
+
+
+    }
 }
